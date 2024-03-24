c66bba69d2d15a82ec854441ee0aa932aa71acd8
DiasZhunistay <zhunistayevdias@gmail.com>
2024-03-24 21:26:50 +0500

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Button from '@mui/material/Button';
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "../action/CounterAction";
+
+export default function Counter() {
+    const dispatch = useDispatch()
+    const count = useSelector((state) => state)
+  return (
+    <div 
+    style={{
+        display: "flex", 
+        justifyContent: "center", 
+        alignItems: "center",
+        minHeight:"100vh",
+    }}
+    >
+        <Button 
+        onClick={() => dispatch(increment())}
+        variant="contained" 
+        size="large"
+        >
+            +
+        </Button>
+        <span>{count}</span>
+        <Button 
+        onClick={() => dispatch(decrement())}
+        variant="contained" 
+        size="large"
+        >
+            -
+        </Button>
+    </div>
+  )
+}
